4b7bea56793bf1327e051574642c4717813b27c9
Einar Darri <einardarrisv@gmail.com>
2021-03-26 17:30:44 +0000

    ok

---
+++ Verkefni 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // breita fyrir player
+    public GameObject Player;
+    // breita fyrir staðsetningu myndarvélar frá player
+    public Vector3 offset = new Vector3(0, 5, -15);
+    // Update is called once per frame
+    void Update()
+    {
+        // myndavélin mun fylgja staðsetninguni á player breituni
+        transform.position = Player.transform.position + offset;
+    }
+}
---
+++ Verkefni 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // breita fyrir hraðan á bílnum
+    public float hradi = 0;
+    public float beygjuHradi = 0f;
+    // input breita
+    private float beygjuInput;
+    private float aframInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // fá Input
+        beygjuInput = Input.GetAxis("Horizontal");
+        aframInput = Input.GetAxis("Vertical");
+        // Færa Bíllinn áfram
+        transform.position += transform.forward * hradi * aframInput;
+        // snúa bílnum
+        transform.Rotate(0, beygjuHradi * beygjuInput, 0);
+    }
+}
