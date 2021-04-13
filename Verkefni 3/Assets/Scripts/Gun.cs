3ef806e3c79685054ad4f8636879e2441ab31a84
Einar Darri <einardarrisv@gmail.com>
2021-04-13 15:43:21 +0000

    byssa fyrir leikman og óvinur sem eltir leikman

---
+++ Verkefni 3/Assets/Scripts/Gun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Prefab fyrir Skot
+    public GameObject Bullet;
+    // breita fyrir hraða á Skoti
+    public float Skot_Hradi = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetKey(KeyCode.Q)){
+            // gera nýjan game object sem er skot
+            GameObject Skot = Instantiate(Bullet, transform.position, transform.rotation) as GameObject;
+            // ná í rigidbody hjá skoti
+            Rigidbody Skot_Rigidbody = Skot.GetComponent<Rigidbody>();
+            // Henda skotinu áfram
+            Skot_Rigidbody.AddForce(transform.forward * Skot_Hradi);
+            // eyða skotinu eftir 5 sec til að filla ekki upp leikinn
+            Destroy(Skot, 5);
+        }   
+    }
+}
---
+++ Verkefni 3/Assets/Scripts/Player_Folow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Folow : MonoBehaviour
+{
+    // beita Fyrir leiman svo óvinur viti hvern á að elta
+    public GameObject Player;
+    public float speed = 0.052f;
+    // það sem Y ásin fyrir rotation þar að vera styltur á til að óvinur snýr að leikmani
+    float rotation;
+    // leingdin á X ás að leikmanni
+    float x;
+    // leingdin á Z ás að leikmanni
+    float z;
+    // fall til að finna út í hvaða átt leikmaður er
+    void find_player()
+    {
+        // x,z er vigur í áttina að leikmanni
+        x = Player.transform.position.x - transform.position.x;
+        z = Player.transform.position.z - transform.position.z;
+        //Debug.Log(x);
+        //Debug.Log(z);
+        // finna gráðurnar fyrir vigurinn 
+        rotation = Mathf.Atan2(x, z) * Mathf.Rad2Deg;
+        //Debug.Log(rotation);
+        transform.rotation = Quaternion.Euler(0, rotation, 0);
+    }
+
+    // Update er kallað 60 sinnum á sec
+    void FixedUpdate()
+    {
+        find_player();
+        transform.position += transform.forward * speed;
+    }
+}
